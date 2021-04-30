2b150d86ec92cd4673177c18b879252ca337a4df
11919159 <e11919159@student.tuwien.ac.at>
2021-04-30 15:50:26 +0200

    added Address and User Dto's

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
@@ -1,19 +1,94 @@
 package at.ac.tuwien.sepm.groupphase.backend.entity;
 
-//TODO: replace this class with a correct ApplicationUser Entity implementation
+import javax.validation.constraints.Past;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ApplicationUser {
 
-    private String email;
-    private String password;
-    private Boolean admin;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public ApplicationUser() {
+    @Column(nullable = false, length = 200)
+    private String firstName;
+
+    @Column(nullable = false, length = 200)
+    private String lastName;
+
+    @Column(nullable = false, length = 200, name = "tel_number")
+    private String telephoneNumber;
+
+    @Column(nullable = false, length = 200)
+    private String email;
+
+    @Column(nullable = false, length = 500)
+    private String password;
+
+    @Column(nullable = false, name = "last_login")
+    @Past
+    private LocalDateTime lastLogin;
+
+    @Column()
+    private int points;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
+    private UserStatus userStatus;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @ManyToOne
+    private Address address;
+
+    public enum UserStatus {
+        ACTIVE, BANNED
     }
 
-    public ApplicationUser(String email, String password, Boolean admin) {
-        this.email = email;
-        this.password = password;
-        this.admin = admin;
+    public enum UserRole {
+        CLIENT, ORGANIZER, ADMIN
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
     }
 
     public String getEmail() {
@@ -32,11 +107,135 @@ public class ApplicationUser {
         this.password = password;
     }
 
-    public Boolean getAdmin() {
-        return admin;
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
     }
 
-    public void setAdmin(Boolean admin) {
-        this.admin = admin;
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public UserStatus getStatus() {
+        return userStatus;
+    }
+
+    public void setStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public static final class UserBuilder {
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String telephoneNumber;
+        private String email;
+        private String password;
+        private LocalDateTime lastLogin;
+        private int points;
+        private UserStatus userStatus;
+        private UserRole role;
+        private Address address;
+
+        private UserBuilder() {
+        }
+
+        public static UserBuilder anUser() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder withTelephoneNumber(String telephoneNumber) {
+            this.telephoneNumber = telephoneNumber;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder withLastLogin(LocalDateTime lastLogin) {
+            this.lastLogin = lastLogin;
+            return this;
+        }
+
+        public UserBuilder withPoints(int points) {
+            this.points = points;
+            return this;
+        }
+
+        public UserBuilder withStatus(UserStatus userStatus) {
+            this.userStatus = userStatus;
+            return this;
+        }
+
+        public UserBuilder withRole(UserRole role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserBuilder withAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public ApplicationUser build() {
+            ApplicationUser user = new ApplicationUser();
+            user.email = this.email;
+            user.lastName = this.lastName;
+            user.password = this.password;
+            user.firstName = this.firstName;
+            user.role = this.role;
+            user.lastLogin = this.lastLogin;
+            user.points = this.points;
+            user.telephoneNumber = this.telephoneNumber;
+            user.address = this.address;
+            user.id = this.id;
+            user.userStatus = this.userStatus;
+            return user;
+        }
     }
 }
