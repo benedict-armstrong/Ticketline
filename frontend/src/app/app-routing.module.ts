2ee9b17a7ebc8d98b87370fa775e8d52aded5e57
11919159 <e11919159@student.tuwien.ac.at>
2021-05-01 13:36:30 +0200

    Updated to Bootstrap 5.0; changed add-user route (add-user insted of addUser)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,32 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ArticleComponent } from './components/article/article.component';
-import { EventDetailComponent } from './components/event-detail/event-detail.component';
-import { EventsComponent } from './components/events/events.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { LoginComponent } from './components/login/login.component';
-import { NewsComponent } from './components/news/news.component';
-import { RegisterComponent } from './components/register/register.component';
-import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
-import { UserHomeComponent } from './components/user-home/user-home.component';
-import { AddUserComponent } from './components/add-user/add-user.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ArticleComponent } from "./components/article/article.component";
+import { EventDetailComponent } from "./components/event-detail/event-detail.component";
+import { EventsComponent } from "./components/events/events.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NewsComponent } from "./components/news/news.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { TicketDetailComponent } from "./components/ticket-detail/ticket-detail.component";
+import { UserHomeComponent } from "./components/user-home/user-home.component";
+import { AddUserComponent } from "./components/add-user/add-user.component";
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'news', component: NewsComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'article', component: ArticleComponent },
-  { path: 'event', component: EventDetailComponent },
-  { path: 'user', component: UserHomeComponent },
-  { path: 'ticket', component: TicketDetailComponent },
-  { path: 'addUser', component: AddUserComponent}
+  { path: "", component: HomePageComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "news", component: NewsComponent },
+  { path: "events", component: EventsComponent },
+  { path: "article", component: ArticleComponent },
+  { path: "event", component: EventDetailComponent },
+  { path: "user", component: UserHomeComponent },
+  { path: "ticket", component: TicketDetailComponent },
+  { path: "add-user", component: AddUserComponent },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" }),
   ],
   exports: [RouterModule],
 })
