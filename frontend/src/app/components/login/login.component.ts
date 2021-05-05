1d5a606fc21862f6fd7b372809f506179e653cdb
Johannes Felzmann <johannes.felzmann@gmx.at>
2021-05-05 11:48:01 +0200

    [US3] added login functionality in frontend

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthRequest } from 'src/app/dtos/auth-request';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  // Error flag
+  error = false;
+  errorMessage = '';
+  
+
+  constructor(private authService: AuthService,private formBuilder: FormBuilder,private router: Router) { 
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+
+    if(this.loginForm.valid){
+      let authObj:AuthRequest = new AuthRequest(
+        this.loginForm.value.email,
+        this.loginForm.value.password
+      );
+
+      this.authService.loginUser(authObj).subscribe(
+        () => {
+          console.log("successful login!");
+          this.router.navigate(['/user']);
+        },
+        error => {
+          this.defaultServiceErrorHandling(error);
+        }
+      );
+    } else {
+      console.log('Invalid input');
+    }
+
+
+  }
+
+  vanishAlert(): void {
+    this.error = false;
+  }
+
+  private defaultServiceErrorHandling(error: any) {
+    console.log(error);
+    this.error = true;
+    if (typeof error.error === 'object') {
+      this.errorMessage = error.error.error;
+    } else {
+      this.errorMessage = error.error;
+    }
   }
 
 }
