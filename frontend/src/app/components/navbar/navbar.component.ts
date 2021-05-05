31cdee5924bb38cbf8a5f8632fb8f405cd78bb4b
11912368 <e11912368@student.tuwien.ac.at>
2021-05-05 21:26:19 +0200

    [US3] added logout functionality

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
   }
 
+  /**
+  * Logout current user.
+  */
+  logoutUser(): void{
+    this.authService.logoutUser();
+  }
+
+  /**
+  * Checks if user is logged in.
+  */
+  isLoggedIn(){
+    return this.authService.isLoggedIn();
+  }
 }
