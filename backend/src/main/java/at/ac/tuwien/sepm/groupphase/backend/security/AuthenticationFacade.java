6158b1c03fa44df5235e701f874a550ee49e3346
11919159 <e11919159@student.tuwien.ac.at>
2021-05-13 12:32:47 +0200

    [General] AuthenticationFacade Documentation; AuthenticationFacade isOrganizer() and isUser() methods

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/security/AuthenticationFacade.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/security/AuthenticationFacade.java
@@ -4,8 +4,31 @@ import org.springframework.security.core.Authentication;
 
 public interface AuthenticationFacade {
 
+    /**
+     *
+     * @return currently authenticated User as Authentication Object (Anonymous if no user is logged in)
+     */
     Authentication getAuthentication();
 
+    /**
+     * Returns true if the User making the request is Authenticated as an Admin
+     *
+     * @return true if the User is Admin false otherwise
+     */
     boolean isAdmin();
+
+    /**
+     * Returns true if the User making the request is Authenticated as an Organizer
+     *
+     * @return true if the User is Organizer false otherwise
+     */
+    boolean isOrganizer();
+
+    /**
+     * Returns true if the User making the request is Authenticated as a User
+     *
+     * @return true if the User is authenticated and not Anonymous false otherwise
+     */
+    boolean isUser();
 
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/security/AuthenticationFacadeImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/security/AuthenticationFacadeImpl.java
@@ -16,4 +16,14 @@ public class AuthenticationFacadeImpl implements AuthenticationFacade {
     public boolean isAdmin() {
         return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
     }
+
+    @Override
+    public boolean isOrganizer() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ROLE_ORGANIZER"));
+    }
+
+    @Override
+    public boolean isUser() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ROLE_USER"));
+    }
 }
