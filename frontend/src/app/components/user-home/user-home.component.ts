9fef0c52bdfbf2061d6f237b879bc9fc631d9837
Johannes Felzmann <e11912368@student.tuwien.ac.at>
2021-05-13 15:26:13 +0200

    refactored login and user-home, added my account to navbar, added password change to user-home

--- frontend/src/app/components/user-home/user-home.component.ts
+++ frontend/src/app/components/user-home/user-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/dtos/user';
 import { AuthService } from 'src/app/services/auth.service';
@@ -7,50 +8,88 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.scss']
+  styleUrls: ['./user-home.component.scss'],
 })
 export class UserHomeComponent implements OnInit {
-
   // Error flag
   error = false;
   errorMessage = '';
+  passwordError = false;
+  passwordErrorMessage = '';
+  // Success Flag
+  passwordSuccess = false;
+
+  // If password change is submitted
+  passwordSubmitted = false;
 
   user: User;
+  userRole: string;
+  passwordChangeForm: FormGroup;
 
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) {
-    if(this.router.getCurrentNavigation().extras.state){
-      this.user = this.router.getCurrentNavigation().extras.state.user;
-    }else{
-      if(authService.isLoggedIn()){
-        this.loadUser(authService.getUserEmail());
-      }else{
-        this.router.navigate(['']);
-      }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    if (this.authService.isLoggedIn()) {
+      this.loadUser(this.authService.getUserEmail());
+    } else {
+      this.router.navigate(['']);
     }
-   }
 
-  ngOnInit(): void {
+    this.passwordChangeForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      passwordRepeat: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
 
-
+  ngOnInit(): void {}
 
   /**
    * Load User with email from Backend.
    */
-  loadUser(email: string){
+  loadUser(email: string) {
     this.userService.getUserByEmail(email).subscribe(
       (response) => {
         this.user = response;
       },
-      error => {
+      (error) => {
         this.defaultServiceErrorHandling(error);
       }
     );
   }
 
+  /**
+   * Password change
+   */
+  changePassword() {
+    this.passwordSubmitted = true;
+    if (this.passwordChangeForm.valid) {
+      if (
+        this.passwordChangeForm.value.password ===
+        this.passwordChangeForm.value.passwordRepeat
+      ) {
+        this.user.password = this.passwordChangeForm.value.password;
+
+        this.userService.updateUser(this.user).subscribe(
+          () => {
+            this.passwordSubmitted = false;
+            this.passwordChangeForm.reset();
+            this.passwordSuccess = true;
+          },
+          (error) => {
+            this.defaultPasswordErrorHandling(error);
+          }
+        );
+      }
+    }
+  }
 
   vanishAlert(): void {
     this.error = false;
+    this.passwordSuccess = false;
+    this.passwordError = false;
   }
 
   private defaultServiceErrorHandling(error: any) {
@@ -63,4 +102,13 @@ export class UserHomeComponent implements OnInit {
     }
   }
 
+  private defaultPasswordErrorHandling(error: any) {
+    console.log(error);
+    this.passwordError = true;
+    if (typeof error.error === 'object') {
+      this.passwordErrorMessage = error.error.error;
+    } else {
+      this.passwordErrorMessage = error.error;
+    }
+  }
 }
