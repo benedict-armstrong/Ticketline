0f04a4a8eaf91c78f74db359c464dd664955d90c
Artem Zhukov <e11843614@student.tuwien.ac.at>
2021-05-13 22:33:43 +0200

    [US5] Show only unread news first

--- frontend/src/app/components/news/news.component.ts
+++ frontend/src/app/components/news/news.component.ts
@@ -1,6 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApplicationNewsService} from '../../services/news.service';
 import {News} from '../../dtos/news';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../dtos/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-news',
@@ -9,19 +12,35 @@ import {News} from '../../dtos/news';
 })
 export class NewsComponent implements OnInit {
 
+  user: User;
+
   news: News[] = [];
-  noNews = false;
+  heldNews: News[] = [];
+  noNews = true;
 
   wasError = false;
   errorMessage: string;
 
+  loadingOnlyUnread = true;
+
   private page = 0;
   private size = 8; // Determines how many news entries are loaded at the beginning and with each click on Load More
 
-  constructor(private newsService: ApplicationNewsService) { }
+  constructor(private newsService: ApplicationNewsService,
+              private authService: AuthService,
+              private userService: UserService) { }
 
   ngOnInit(): void {
     this.loadBatch();
+    this.userService.getUserByEmail(
+      this.authService.getUserEmail()
+    ).subscribe(
+      user => {
+        this.user = user;
+      }, error => {
+        console.log(error);
+      }
+    );
   }
 
   /**
@@ -30,16 +49,44 @@ export class NewsComponent implements OnInit {
    * Offsetting is done with the help of IDs.
    */
   loadBatch() {
+    if (!this.loadingOnlyUnread && this.heldNews.length > 0) {
+      this.news.push(...this.heldNews);
+      this.heldNews = [];
+    }
     this.wasError = false;
     this.newsService.getNews(this.page, this.size).subscribe(
       response => {
-        this.news.push(...response);
+        const unread = response.filter(item => item.id > this.user.lastReadNews.id);
+        const read = response.filter(item => item.id <= this.user.lastReadNews.id);
+        this.news.push(...unread);
+
+        if (unread.length < this.size) {
+          this.loadingOnlyUnread = false;
+          this.heldNews.push(...read);
+        } else {
+          this.loadingOnlyUnread = true;
+        }
+
         if (response.length > 0) {
           this.page++;
+          this.noNews = false;
         }
         if (response.length < this.size) {
           this.noNews = true;
         }
+
+      }, error => {
+        this.handleError(error);
+      }
+    );
+  }
+
+  markAllAsRead() {
+    this.user.lastReadNews = this.news[0];
+    console.log('USR', this.user);
+    this.userService.updateUser(this.user).subscribe(
+      user => {
+        this.user = user;
       }, error => {
         this.handleError(error);
       }
