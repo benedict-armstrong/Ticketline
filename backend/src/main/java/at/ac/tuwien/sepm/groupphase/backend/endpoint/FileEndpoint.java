cb1687535bc095d41aa2da843919ecb266929108
Artem Zhukov <e11843614@student.tuwien.ac.at>
2021-05-13 12:38:05 +0200

    Annotate endpoints with @Secured

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/FileEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/FileEndpoint.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,8 +37,8 @@ public class FileEndpoint {
         this.fileMapper = fileMapper;
     }
 
-    @PermitAll
     @PostMapping
+    @Secured("ROLE_ORGANIZER")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Upload a file")
     public FileDto uploadFile(@RequestParam("file") MultipartFile file) {
