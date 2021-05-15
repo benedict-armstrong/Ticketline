116e261da8cb1e7e9fcc3b0015b6616c880b5dd0
Johannes Felzmann <e11912368@student.tuwien.ac.at>
2021-05-15 09:28:11 +0200

    added again event classes commentted out

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/EventRepository.java
@@ -0,0 +1,24 @@
+/*
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    /**
+     * Find all event entries.
+     *
+     * @return ordered list of all events
+     */
+
+/*
+    List<Event> findAll();
+
+}
+
+ */
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/EventServcie.java
@@ -0,0 +1,28 @@
+/*
+package at.ac.tuwien.sepm.groupphase.backend.service;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+
+    /**
+     * Find all events.
+     *
+     * @return list of all events
+     */
+/*
+    List<Event> findAll();
+
+    /**
+     * Find event by id.
+     *
+     * @return event with id
+     */
+/*
+    Event findById(long id);
+}
+
+
+ */
