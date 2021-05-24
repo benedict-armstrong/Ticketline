04789578d0176d96cd9160364e7d5d305a5b23bb
11919159 <e11919159@student.tuwien.ac.at>
2021-05-24 12:53:14 +0200

    updated UserDataGenerator

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/VenueDataGenerator.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.groupphase.backend.datagenerator;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("generateData")
+@Component
+public class VenueDataGenerator {
+
+}
