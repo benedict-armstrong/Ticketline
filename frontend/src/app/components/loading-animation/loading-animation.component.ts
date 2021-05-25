7271a89da20e92b15e5bed8d2b60230e60ef9cb3
11919159 <e11919159@student.tuwien.ac.at>
2021-05-25 18:04:40 +0200

    Linting in Frontend

--- frontend/src/app/components/loading-animation/loading-animation.component.ts
+++ frontend/src/app/components/loading-animation/loading-animation.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-loading-animation",
+  selector: 'app-loading-animation',
   template: ` <div class="lds-ellipsis">
     <div></div>
     <div></div>
     <div></div>
     <div></div>
   </div>`,
-  styleUrls: ["./loading-animation.component.scss"],
+  styleUrls: ['./loading-animation.component.scss'],
 })
 export class LoadingAnimationComponent implements OnInit {
   constructor() {}
