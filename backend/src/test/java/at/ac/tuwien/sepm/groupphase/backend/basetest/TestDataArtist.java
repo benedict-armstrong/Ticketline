196a4946f299890fe1fe30cab1ba9ecb834fdb69
= <e11902102@student.tuwien.ac.at>
2021-05-27 15:27:35 +0200

    [Tests] Created Testdata and first tests for Mapper

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestDataArtist.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestDataArtist.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.basetest;
 
 public interface TestDataArtist extends TestData {
     String TEST_ARTIST_FIRSTNAME = "Max";
+    String TEST_ARTIST_FIRSTNAME2 = "Melina";
     String TEST_ARTIST_LASTNAME = "Musterkuenstler";
 
     String ARTIST_BASE_URI = BASE_URI + "/artists";
