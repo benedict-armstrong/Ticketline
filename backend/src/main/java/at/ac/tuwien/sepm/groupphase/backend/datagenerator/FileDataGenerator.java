6b8ace4d93b08aee69ecdd54cdd41a9815e023f9
Artem Zhukov <e11843614@student.tuwien.ac.at>
2021-05-31 20:23:53 +0200

    Adjust file generator to download images

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/FileDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/FileDataGenerator.java
@@ -8,34 +8,39 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.net.URL;
 
 @Profile("generateData")
 @Component
 public class FileDataGenerator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private static final int NUMBER_OF_FILES_TO_GENERATE = 5;
-    private static final int MAXIMUM_LENGTH_OF_FILE_DATA = 50;
-
     private final FileRepository fileRepository;
+
+    // Free images from pixabay.com
+    private static final String[] imageUrls = new String[]{
+        "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png",
+        "https://cdn.pixabay.com/photo/2015/11/22/19/04/crowd-1056764_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2016/11/22/19/15/hand-1850120_960_720.jpg"
+    };
 
     public FileDataGenerator(FileRepository fileRepository) {
         this.fileRepository = fileRepository;
     }
 
     @PostConstruct
-    private void generateFiles() {
+    private void generateFiles() throws Exception {
         if (fileRepository.findAll().size() > 0) {
             LOGGER.debug("Files have already been generated");
         } else {
-            LOGGER.debug("Generating {} files", NUMBER_OF_FILES_TO_GENERATE);
-            for (int i = 0; i < NUMBER_OF_FILES_TO_GENERATE; i++) {
+            LOGGER.debug("Generating {} files", imageUrls.length);
+            for (String imageUrl : imageUrls) {
                 File file = File.builder()
-                    .data(randomData())
-                    .type(randomFileType())
+                    .data(download(imageUrl))
+                    .type(getTypeFrom(imageUrl))
                     .build();
                 LOGGER.debug("Saving file {}", file);
                 fileRepository.save(file);
@@ -43,15 +48,37 @@ public class FileDataGenerator {
         }
     }
 
-    private byte[] randomData() {
-        byte[] data = new byte[ThreadLocalRandom.current().nextInt(MAXIMUM_LENGTH_OF_FILE_DATA)];
-        new Random().nextBytes(data);
-        return data;
+    private byte[] download(String urlText) throws Exception {
+        LOGGER.trace("download({})", urlText);
+        URL url = new URL(urlText);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int n;
+            byte [] buffer = new byte[1024];
+            while (-1 != (n = inputStream.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+        }
+        return output.toByteArray();
     }
 
-    private File.Type randomFileType() {
-        File.Type[] fileTypes = File.Type.values();
-        return fileTypes[ThreadLocalRandom.current().nextInt(fileTypes.length)];
+    private File.Type getTypeFrom(String url) {
+        LOGGER.trace("getTypeFrom({})", url);
+        int extensionDotIndex = url.length() - 1;
+        for (int i = extensionDotIndex; i >= 0; i--) {
+            if (url.charAt(i) == '.') {
+                break;
+            }
+        }
+        String extension = url.substring(extensionDotIndex + 1);
+        switch (extension) {
+            case "jpg":
+                return File.Type.IMAGE_JPG;
+            case "png":
+                return File.Type.IMAGE_PNG;
+            default:
+                throw new IllegalArgumentException("Couldn't map extension to file type for downloading");
+        }
     }
 
 }
