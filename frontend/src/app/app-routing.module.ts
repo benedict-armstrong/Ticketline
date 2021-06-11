4d84292de487422f7fa7c37a4f6453eb3d07bd86
11919159 <e11919159@student.tuwien.ac.at>
2021-06-11 20:11:39 +0200

    [US 20] UI tweaks; basic explore page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,84 +1,84 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { EventDetailComponent } from "./components/event-detail/event-detail.component";
-import { SearchResultComponent } from "./components/search/search-result/search-result.component";
-import { HomePageComponent } from "./components/home-page/home-page.component";
-import { LoginComponent } from "./components/login/login.component";
-import { NewsComponent } from "./components/news/news.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { TicketDetailComponent } from "./components/ticket-detail/ticket-detail.component";
-import { UserHomeComponent } from "./components/user-home/user-home.component";
-import { AddNewsComponent } from "./components/add-news/add-news.component";
-import { NewsDetailComponent } from "./components/news-detail/news-details.component";
-import { AddUserComponent } from "./components/add-user/add-user.component";
-import { AuthGuard } from "./guards/auth.guard";
-import { AddEventComponent } from "./components/event/add-event/add-event.component";
-import { PerformanceDetailComponent } from "./components/performance-detail/performance-detail.component";
-import { BookingComponent } from "./components/booking/booking.component";
-import { AddressDetailComponent } from "./components/search/address-detail/address-detail.component";
-import { ArtistDetailComponent } from "./components/search/artist-detail/artist-detail.component";
-import { AddVenueComponent } from "./components/venue/add-venue/add-venue.component";
-import { ExploreComponent } from "./components/explore/explore.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { SearchResultComponent } from './components/search/search-result/search-result.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewsComponent } from './components/news/news.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
+import { AddNewsComponent } from './components/add-news/add-news.component';
+import { NewsDetailComponent } from './components/news-detail/news-details.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AddEventComponent } from './components/event/add-event/add-event.component';
+import { PerformanceDetailComponent } from './components/performance-detail/performance-detail.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { AddressDetailComponent } from './components/search/address-detail/address-detail.component';
+import { ArtistDetailComponent } from './components/search/artist-detail/artist-detail.component';
+import { AddVenueComponent } from './components/venue/add-venue/add-venue.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
 const routes: Routes = [
-  { path: "", component: HomePageComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "news", component: NewsComponent },
-  { path: "explore", component: ExploreComponent },
-  { path: "events", component: SearchResultComponent },
-  { path: "news-detail/:id", component: NewsDetailComponent },
-  { path: "address-detail/:id", component: AddressDetailComponent },
-  { path: "artist-detail/:id", component: ArtistDetailComponent },
-  { path: "event-detail/:id", component: EventDetailComponent },
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'events', component: SearchResultComponent },
+  { path: 'news-detail/:id', component: NewsDetailComponent },
+  { path: 'address-detail/:id', component: AddressDetailComponent },
+  { path: 'artist-detail/:id', component: ArtistDetailComponent },
+  { path: 'event-detail/:id', component: EventDetailComponent },
   {
-    path: "add-venue",
+    path: 'add-venue',
     component: AddVenueComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER'] },
   },
-  { path: "performance-detail/:id", component: PerformanceDetailComponent },
+  { path: 'performance-detail/:id', component: PerformanceDetailComponent },
   {
-    path: "user",
+    path: 'user',
     component: UserHomeComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER", "USER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER', 'USER'] },
   },
   {
-    path: "ticket",
+    path: 'ticket',
     component: TicketDetailComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER", "USER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER', 'USER'] },
   },
   {
-    path: "add-news/:id",
+    path: 'add-news/:id',
     component: AddNewsComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER'] },
   },
   {
-    path: "add-user",
+    path: 'add-user',
     component: AddUserComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN"] },
+    data: { roles: ['ADMIN'] },
   },
   {
-    path: "add-event",
+    path: 'add-event',
     component: AddEventComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER'] },
   },
   {
-    path: "orders",
+    path: 'orders',
     component: BookingComponent,
     canActivate: [AuthGuard],
-    data: { roles: ["ADMIN", "ORGANIZER", "USER"] },
+    data: { roles: ['ADMIN', 'ORGANIZER', 'USER'] },
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" }),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
   ],
   exports: [RouterModule],
 })
