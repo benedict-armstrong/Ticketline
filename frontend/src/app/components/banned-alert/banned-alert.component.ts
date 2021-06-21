c062e40f8552a160456ac1d4213dfcecf1fbdb4a
Artem Zhukov <e11843614@student.tuwien.ac.at>
2021-06-21 17:26:33 +0200

    [US20] Split auth and status guards

--- frontend/src/app/components/banned-alert/banned-alert.component.ts
+++ frontend/src/app/components/banned-alert/banned-alert.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../services/auth.service';
-import {UserService} from '../../services/user.service';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-banned-alert',
@@ -10,23 +7,8 @@ import {Router} from '@angular/router';
 })
 export class BannedAlertComponent implements OnInit {
 
-  constructor(private authService: AuthService,
-              private userService: UserService,
-              private router: Router) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    if (!this.authService.isLoggedIn()) {
-      this.router.navigate(['/']);
-    }
-    this.userService.getUserByEmail(this.authService.getUserEmail()).subscribe(
-      response => {
-        if (response.status !== 'BANNED') {
-          this.router.navigate(['/']);
-        }
-      }, error => {
-        console.error(error);
-      }
-    );
-  }
+  ngOnInit(): void { }
 
 }
--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {CanActivate, Router, ActivatedRouteSnapshot} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {UserService} from '../services/user.service';
-import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,33 +12,17 @@ export class AuthGuard implements CanActivate {
               private userService: UserService,
               private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
     if (this.authService.isLoggedIn()) {
-      // Check if has permissions
       if (this.checkPermissions(route)) {
-        // Check if banned
-        this.userService.getUserByEmail(this.authService.getUserEmail()).subscribe(
-          response => {
-            if (response.status === 'BANNED') {
-              console.log('No access due to ban');
-              this.router.navigate(['/banned']);
-              return false;
-            } else {
-              return true;
-            }
-          }, error => {
-            console.error(error);
-            return false;
-          }
-        );
-        return of(true);
+        return true;
       } else {
         this.router.navigate(['']);
-        return of(false);
+        return false;
       }
     } else {
       this.router.navigate(['/login']);
-      return of(false);
+      return false;
     }
   }
 
